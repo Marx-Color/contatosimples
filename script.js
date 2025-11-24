fcce4574e03d60262c56fde45666fd022bcc5d82
Reinaldo Duarte <reicd1@gmail.com>
2025-11-24 10:59:32 -0300

    rever
    executar

---
+++ script.js
@@ -0,0 +1,13 @@
+function tooleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+  //pegar a tag img
+  //Substituir a imagem
+  cons
+   if(html.classList.contains('light')){
+       html.classList.remove ('ligtt')
+
+     } elese{
+       html.classList.add('light')
+     }
+}
